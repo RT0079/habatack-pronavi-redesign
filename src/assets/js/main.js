4e6800c4b4aa6bcc1b173270294c58389358398e
mikatanaka <tanaka243158@gmail.com>
2025-11-26 02:10:37 +0900

    途中スライド

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("ProNavi Redesign - Team Habatack");
 });
+ $(".slide-item").slick({
+  slidesToShow: 1,
+  centerMode: true,
+  centerPadding: '0px',
+  arrows: true,
+ });
